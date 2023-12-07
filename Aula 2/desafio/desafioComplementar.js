f43d6ae92381174a21b6c678261d9326ed916aed
Lucas Moura <lucasrmoura94@gmail.com>
2023-12-06 21:56:17 -0300

    entrega desafio complementar

---
+++ Aula 2/desafio/desafioComplementar.js
@@ -0,0 +1,11 @@
+// 1º desafio complementar coder house - Aula 2
+
+nome = prompt("Bem vindo, digite seu nome:")
+sobrenome = prompt("Qual o seu sobrenome:")
+anoDeNascimento = prompt("Em que ano você nasceu?")
+anoAtual = 2023
+
+idade = parseInt(anoAtual) - parseInt(anoDeNascimento) 
+
+alert("Olá, " + nome + " " + sobrenome + "! Este ano você fez ou irá fazer " + idade + " anos de idade."  )
+
